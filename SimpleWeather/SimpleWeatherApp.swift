2131a6528d2b50782d43176b1fb675ebb6b592d5
Philip Keller <philipjoelkeller@gmail.com>
2023-03-02 11:51:12 -0800

    Initial Commit

---
+++ SimpleWeather/SimpleWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherApp.swift
+//  SimpleWeather
+//
+//  Created by Philip Keller on 3/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
